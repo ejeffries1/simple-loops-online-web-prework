04d86bc9df7316c6e0cad7d6d5a832647ef6a2b5
Evan Jeffries <ejeffries14@gmail.com>
2019-06-05 10:04:43 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,7 +11,7 @@ def loop_iterator(number_of_times)
   number_of_times = 0
   loop do
     number_of_times += 1
-    if number_of_times <= 7
+    if number_of_times >= 7
       puts "#{phrase}"
       break
   end
