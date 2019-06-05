c54df70f7f9fa5398c5f5413beb25019c056bc08
Evan Jeffries <ejeffries14@gmail.com>
2019-06-05 09:39:36 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -15,6 +15,7 @@ def times_iterator(number_of_times)
   number_of_times = 0
   7.times do
     puts "#{phrase}"
+    number_of_times = number_of_times - 1
   phrase = "Welcome to Flatiron School's Web Development Course!"
 end
 
