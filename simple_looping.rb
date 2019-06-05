5e294ed381e375603c3728192adea80706bbe99a
Evan Jeffries <ejeffries14@gmail.com>
2019-06-05 09:36:38 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -12,6 +12,7 @@ end
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
+  number_of_times = 0
   phrase = "Welcome to Flatiron School's Web Development Course!"
 end
 
