e6addcf14a0677acf0ad090636d287390537981a
Evan Jeffries <ejeffries14@gmail.com>
2019-06-05 10:56:42 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -10,10 +10,7 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
   loop do
-    counter <= number_of_times
-    puts "#{phrase}"
-    counter += 1
-    if counter >= number_of_times
+   
     break
   end
 end
